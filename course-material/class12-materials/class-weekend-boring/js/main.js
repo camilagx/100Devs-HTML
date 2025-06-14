5a524c6bb98ed9f7ea025d4d8cd3489f07675fec
Camila <camilax.gomez97@gmail.com>
2025-06-13 23:10:48 -0700

    Completed the class-weekend-boring exercise

--- course-material/class12-materials/class-weekend-boring/js/main.js
+++ course-material/class12-materials/class-weekend-boring/js/main.js
@@ -2,9 +2,21 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase();
+  h2Element = document.querySelector('#placeToSee');
 
+  let msg;
   //Conditionals go here
+  if (day === "monday" || day === "tuesday") {
+    msg = "It's a boring day.";
+  } else if (day === "wednesday" || day === "thursday") {
+    msg = "It's class day!";
+  } else if (day === "friday" || day === "saturday") {
+    msg = "It's a WEEKEND!!";
+  } else {
+    msg = "It's REST DAY.";
+  }
 
+  h2Element.innerText = msg;
 
 }
